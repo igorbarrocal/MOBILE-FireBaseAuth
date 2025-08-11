04132179a19c1770a9ec3319df4822af1c9ab98f
igorbarrocal <igorbarrocal@hotmail.com>
2025-08-11 11:15:36 -0300

    configurando autenticacao

--- services/firebaseConfig.tsx
+++ services/firebaseConfig.tsx
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA4CRzI39qB95rS1UX7cTh6PuNP1IHsldU",
@@ -12,3 +13,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
