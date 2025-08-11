e039e5c591822f507b6e351470c8d76d16e973ce
igorbarrocal <igorbarrocal@hotmail.com>
2025-08-11 10:56:51 -0300

    Adicionando o firebase para o projeto

---
+++ services/firebaseConfig.tsx
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4CRzI39qB95rS1UX7cTh6PuNP1IHsldU",
+  authDomain: "projetoaulaautenticacao.firebaseapp.com",
+  projectId: "projetoaulaautenticacao",
+  storageBucket: "projetoaulaautenticacao.firebasestorage.app",
+  messagingSenderId: "530213090198",
+  appId: "1:530213090198:web:0995d800497d02161823b7"
+};
+
+
+const app = initializeApp(firebaseConfig);
