79d6d7a409d3f30cf77d15c49bfa06f55c1d0503
igorbarrocal <igorbarrocal@hotmail.com>
2025-08-13 09:03:56 -0300

    Button do Logout

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,10 +1,20 @@
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import  AsyncStorage  from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+
 
 export default function HomeScreen(){
+    const router = useRouter() //Hook de navegacao
+    const realizarLogoff = async()=>{
+        await AsyncStorage.removeItem('@user')
+        router.push('./')//Redireciona para index.tsx
+    }
+    
     return(
         <SafeAreaView>
             <Text>Seja bem-vindo - Você está Logado!!</Text>
+            <Button title="Sair da conta" onPress={} />
         </SafeAreaView>
     )
 }
